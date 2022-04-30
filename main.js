53a13130e6d2471ba78aeddcc5ff32f0a61b8c8b
Renato Lima <66927604+eurenatolima@users.noreply.github.com>
2022-04-29 23:17:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -32,3 +32,13 @@ header.classList.add('scroll')
 header.classList.remove('scroll')
 }
 })
+
+/*testimonials carousel sliders*/
+const swiper = new Swiper('.swiper-container', {
+slidesPerView: 1,
+pagination: {
+    el: '.swiper-pagination'
+},
+mousewheel: true,
+keyboard: true
+})
